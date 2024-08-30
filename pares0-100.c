b17a7c85b04477927d64cf36245ab2ac9fd0830d
AngelBobadilla <angelpollito2007@gmail.com>
2024-08-30 14:50:12 -0600

    Create pares0-100.c

---
+++ pares0-100.c
@@ -0,0 +1,19 @@
+/* Angel Amado Bobadilla Galva
+Practica 3 - programa suma de o a 100*/
+#include <stdio.h>
+#include <math.h>
+
+int main ()
+{
+  printf ("Los numeros pares entre 0 y 100 son: ");
+
+  for (i = 0; i<=100; i++)
+  {
+    if (i % 2== 0)
+    {
+        printf(" %d ", i);
+    }
+{
+  printf(" \n ");
+return o;
+}
