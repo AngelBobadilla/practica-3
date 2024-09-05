06a35c6755062d6586f6e4d0c26b4d6f64c5c9b4
AngelBobadilla <angelpollito2007@gmail.com>
2024-09-05 11:19:31 -0600

    Update pares0-100.c

--- pares0-100.c
+++ pares0-100.c
@@ -3,17 +3,18 @@ Practica 3 - programa suma de o a 100*/
 #include <stdio.h>
 #include <math.h>
 
+int i, temp;
 int main ()
 {
-  printf ("Los numeros pares entre 0 y 100 son: ");
+  printf (" Los numeros pares entre 0 y 100son: ");
 
-  for (i = 0; i<=100; i++)
+  for (i = 0; i <=100; i++)
   {
-    if (i % 2== 0)
-    {
+      if (i % 2 == 0)
+      {
         printf(" %d ", i);
-    }
+
+        printf(" \n ", i);
+      }
+return 0;
 {
-  printf(" \n ");
-return o;
-}
